06e2e6d059b6ac5f943b2e91d73b30a05b817858
leah-wang-design <leahwangdesign@users.noreply.github.com>
2021-05-13 00:31:18 -0400

    fontweightchanged to 300, homepage color changed to gradient

--- work.js
+++ work.js
@@ -38,9 +38,9 @@ $(document).scroll(function() {
     console.log("mouse" + z);
     if (y > z) {
 
-        $('.mousesection').fadeOut({ duration: 1200 });
+        $('.mousesection').css('opacity', '0');
     } else {
-        $('.mousesection').fadeIn({ duration: 800 });
+        $('.mousesection').css('opacity', '100');
     }
 });
 
@@ -63,3 +63,33 @@ $(document).ready(function() {
         function() { $(this).attr("src", "github-01.svg"); }
     )
 });
+
+$(window).scroll(function() {
+
+    // selectors
+    var $window = $(window),
+        $body = $('.bodycolor'),
+        $panel = $('.panel');
+
+    // Change 33% earlier than scroll position so colour is there when you arrive.
+    var scroll = $window.scrollTop() + ($window.height() / 3);
+
+    $panel.each(function() {
+        var $this = $(this);
+
+        // if position is within range of this panel.
+        // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+        // Remember we set the scroll to 33% earlier in scroll var.
+        if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+
+            // Remove all classes on body with color-
+            $body.removeClass(function(index, css) {
+                return (css.match(/(^|\s)color-\S+/g) || []).join(' ');
+            });
+
+            // Add class of currently active div
+            $body.addClass('color-' + $(this).data('color'));
+        }
+    });
+
+}).scroll();
